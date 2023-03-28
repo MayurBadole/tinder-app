f3476c104421afb0de0df3ea2c147c53ce79b784
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-28 12:21:15 +0530

    adding img

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 import "./Header.css";
-import PersonIcon from '@mui/icons-material/Person';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import ForumIcon from '@mui/icons-material/Forum';
+import PersonIcon from "@mui/icons-material/Person";
+import ForumIcon from "@mui/icons-material/Forum";
 const Header = () => {
   return (
-    <div className='header'>
-        <PersonIcon/>
-        <WhatshotIcon/>
-        <ForumIcon/>
+    <div className="header">
+      <PersonIcon fontSize="large" className="header__icon" />
+      <img
+        className="header__logo"
+        src="https://w7.pngwing.com/pngs/698/493/png-transparent-tinder-hd-logo-thumbnail.png"
+        alt="tinder-logo"
+      />
 
-    
-    
-    
+      <ForumIcon fontSize='large' className="header_icon" />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
