e8fb109ad58040627fae750ec0e18d4265f0bfd0
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-28 12:48:39 +0530

    card profile add file

---
+++ src/TinderCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TinderCard.css';
+const TinderCard = () => {
+  return (
+    <div className='tinderCard'>
+    TinderCard</div>
+  )
+}
+
+export default TinderCard
