a4e9f4288736b8a5d06d1ec00b324dde15fc27ff
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-28 11:33:17 +0530

    adding material ui

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import "./Header.css";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
 const Header = () => {
   return (
-    <div className='header'>Header
+    <div className='header'>
+        <AccountCircleIcon/>
+        <WhatshotIcon/>
+
     
     
     
