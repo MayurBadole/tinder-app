30d65c3362ee9abc4f3a83af99dfe2d43b555130
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-29 09:18:47 +0530

    adding material ui  icon

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./Header.css";
-import PersonIcon from "@mui/icons-material/Person";
-import ForumIcon from "@mui/icons-material/Forum";
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
 import { IconButton } from "@mui/material";
 const Header = () => {
   return (
     <div className="header">
       <IconButton>
-      <PersonIcon fontSize="large" className="header__icon" />
+        <PersonIcon fontSize="large" className="header__icon" />
       </IconButton>
       <img
         className="header__logo"
         src="https://w7.pngwing.com/pngs/698/493/png-transparent-tinder-hd-logo-thumbnail.png"
         alt="tinder-logo"
       />
-    <IconButton>
-      <ForumIcon fontSize='large' className="header_icon" />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
       </IconButton>
     </div>
   );
 };
 
 export default Header;
+ 
