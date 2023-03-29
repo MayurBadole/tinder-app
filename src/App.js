9ec736d76279690a9eb84bbca186a0795ba9f918
Mayur Badole <115920456+MayurBadole@users.noreply.github.com>
2023-03-29 09:18:00 +0530

    adding swipebutton component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
+import Header from './Header.js';
+import SwipeButtons from './SwipeButtons';
+import TinderCards from "./TinderCards.js"
 
 function App() {
   return (
     <div className="app">
       <Header/> 
+      <TinderCards/>
+      <SwipeButtons/>
     </div>
   );
 }
